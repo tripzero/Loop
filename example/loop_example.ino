74e42cbc70afb273f590a404b05c600dc35c51d2
Kevron Rees <tripzero.kev@gmail.com>
2015-08-30 17:36:11 -0700

    added example

---
+++ example/loop_example.ino
@@ -0,0 +1,16 @@
+#include <Loop/Loop.h>
+
+Loop *loop = Loop::instance();
+
+void setup()
+{
+	//Time Sync Every day
+	loop.addTimeout(1000 * 60 * 60 * 24, [](){
+		Spark.syncTime();
+	});
+}
+
+void loop()
+{
+	loop.process();
+}
